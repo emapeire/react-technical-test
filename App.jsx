c204e31100160fef9f9436b62d98d9495b094682
Emanuel Peire <emapeire@outlook.com>
2023-11-12 18:55:38 -0300

    Add React app files and configuration

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import React from 'react'
+import { createRoot } from 'react-dom'
+
+createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <h1>Hello, world!</h1>
+  </React.StrictMode>
+)
