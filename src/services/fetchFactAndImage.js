4a2e06157647299796518101b8431b18db42d462
Emanuel Peire <emapeire@outlook.com>
2023-11-13 18:25:27 -0300

    Add error handling for fetch request in fecthFactAndImage function

--- src/services/fetchFactAndImage.js
+++ src/services/fetchFactAndImage.js
@@ -4,6 +4,7 @@ const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
 export const fecthFactAndImage = async () => {
   try {
     const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+    if (!res.ok) throw new Error('Error fetching data')
     const data = await res.json()
     const { fact } = data
 
