a7eff1b94f39223c672515437c09191edd119f8e
Emanuel Peire <emapeire@outlook.com>
2023-11-12 23:37:55 -0300

    Update CSS and import App component.

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
 import './index.css'
 import React from 'react'
-import { createRoot } from 'react-dom'
+import { createRoot } from 'react-dom/client'
+import App from './src/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>hola</h1>
+    <App />
   </React.StrictMode>
 )
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable space-before-function-paren */
 export default function App() {
   return (
     <div>
